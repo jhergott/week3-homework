211202699417231f13ca6de40e48d954b0b21584
Jake Hergott <jakehergott@gmail.com>
2015-04-19 11:24:09 -0500

    challenge 3 completed

--- chicago/app/controllers/places_controller.rb
+++ chicago/app/controllers/places_controller.rb
@@ -1,5 +1,32 @@
 class PlacesController < ApplicationController
 
+  def new
+    # do nothing
+  end
+
+  def edit
+    @place_to_edit = Place.find_by(:id => params["id"])
+  end
+
+  def update
+    place_update = Place.find_by(:id => params["id"])
+    place_update.title = params["title"]
+    place_update.photo_url = params["url"]
+    place_update.price = params["price"].to_i
+    place_update.description = params["description"]
+    place_update.save
+    redirect_to "/places/#{place_update.id}"
+  end
+
+  def create
+    title = params["title"]
+    url = params["url"]
+    price = params["price"].to_i
+    description = params["description"]
+    Place.create(title: title, photo_url: url, price: price, description: description)
+    redirect_to "/places"
+  end
+
   def index
     @places = Place.all
   end
@@ -13,5 +40,7 @@ class PlacesController < ApplicationController
     place_to_delete.delete
     redirect_to "/places"
   end
+
+
 
 end
--- chicago/config/routes.rb
+++ chicago/config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
 
   # Put your routes here
+  get '/places/new' => 'places#new'
+  get '/places/create' => 'places#create'
+
+  get '/places/:id/edit' => 'places#edit'
+  get '/places/:id/update' => 'places#update'
+
   get '/places' => 'places#index'
   get '/places/:id' => 'places#show'
   get '/places/:id/delete' => 'places#delete'
+  
+  
 
 end
